a9622c174f4ddc0f647625e3bad6d4bfee775e14
Melle <melle@git.com>
2024-01-07 20:58:11 +0100

    Change way of importing cdk

--- bin/mwaa-airflow.ts
+++ bin/mwaa-airflow.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import { MwaaAirflowStack } from "../lib/mwaa-airflow-stack";
 
-const app = new cdk.App();
+const app = new App();
 new MwaaAirflowStack(app, "MwaaAirflowStack");
